18776ca552f2695f60d0d72cf840af008dcf6de2
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-01-20 16:44:20 +0530

    my nav and foot

---
+++ componant/navbar.js
@@ -0,0 +1,25 @@
+function navbar(){
+
+    return ` <div class="navbar">
+    <div class="leftnav">
+        <div id="revimg">
+            <img src="https://www.revv.co.in/grapheneImages/newopen/logo.svg" alt="" height="100%" width="100%">
+        </div>
+    </div>
+    <div class="rightnav">
+        <div><a href=""><b>Hyundai</b> Subscription</a></div>
+        <div><a href=""><b>Mahindra</b> Subscription</a></div>
+        <div id="faqIs"><a href="">FAQs</a></div>
+        <div id="logIs">
+            <div id="iconimg">
+               <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbeuZBQhGnf6CkSx8P92YptbTnuWKIrsiODQ&usqp=CAU" alt="" height="100%" width="100%">
+            </div>
+            <a href="">Login or Signup</a>
+        </div>
+
+    </div>
+
+</div>`
+}
+
+export default navbar;
